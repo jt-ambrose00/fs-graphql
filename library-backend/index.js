6d8529d0c1ace57f7d79c05d8dbc5408b72a6927
JA8866 <109983530+JA8866@users.noreply.github.com>
2026-05-21 20:20:11 -0500

    finished chapter 2 exercise 6

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v4: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
 let authors = [
   {
@@ -84,22 +86,6 @@ let books = [
   },
 ]
 
-// const typeDefs = `
-//   type Person {
-//     name: String!
-//     phone: String
-//     street: String!
-//     city: String! 
-//     id: ID!
-//   }
-
-//   type Query {
-//     personCount: Int!
-//     allPersons: [Person!]!
-//     findPerson(name: String!): Person
-//   }
-// `
-
 const typeDefs = /* GraphQL */ `
   type Author {
     name: String!
@@ -122,16 +108,21 @@ const typeDefs = /* GraphQL */ `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
-`
 
-// const resolvers = {
-//   Query: {
-//     personCount: () => persons.length,
-//     allPersons: () => persons,
-//     findPerson: (root, args) =>
-//       persons.find(p => p.name === args.name)
-//   }
-// }
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+
+    # addAuthor(
+    #   name: String!
+    #   born: Int
+    # ): Author
+  }
+`
 
 const resolvers = {
   Query: {
@@ -157,6 +148,35 @@ const resolvers = {
       )
     }
   },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (books.find(book => book.title === args.title)) {
+        throw new GraphQLError(`Title must be unique: ${args.title}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.title
+          }
+        })
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+
+      if (!authors.find(author => author.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+
+      return book
+    },
+
+    // addAuthor: (root, args) => {
+    //   const author = { ...args, id: uuid() }
+    //   authors = authors.concat(author)
+    //   return author
+    // },
+  },
 }
 
 const server = new ApolloServer({
@@ -169,3 +189,56 @@ startStandaloneServer(server, {
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
+
+// Implement mutation addBook, which can be used like this:
+
+// mutation {
+//   addBook(
+//     title: "NoSQL Distilled",
+//     author: "Martin Fowler",
+//     published: 2012,
+//     genres: ["database", "nosql"]
+//   ) {
+//     title,
+//     author
+//   }
+// }
+
+// The mutation works even if the author is not already saved to the server:
+
+// mutation {
+//   addBook(
+//     title: "Pimeyden tango",
+//     author: "Reijo Mäki",
+//     published: 1997,
+//     genres: ["crime"]
+//   ) {
+//     title,
+//     author
+//   }
+// }
+
+// If the author is not yet saved to the server, a new author is added to the system. The birth years of authors are not saved to the server yet, so the query
+
+// query {
+//   allAuthors {
+//     name
+//     born
+//     bookCount
+//   }
+// }
+
+// returns
+
+// {
+//   "data": {
+//     "allAuthors": [
+//       // ...
+//       {
+//         "name": "Reijo Mäki",
+//         "born": null,
+//         "bookCount": 1
+//       }
+//     ]
+//   }
+// }
